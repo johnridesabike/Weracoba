3671e93b61ae979fe88f4e8611254d51d7eec216
John <37978984+johnridesabike@users.noreply.github.com>
2018-08-10 16:47:31 -0400

    More updates!!!

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,7 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
         <div class="entry-header-wrap">
-		  <?php the_title( '<h1 class="entry-title screen-reader-text">', '</h1>' ); ?>
+		  <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
         </div>
 	</header><!-- .entry-header -->
 
