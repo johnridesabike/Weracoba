0fa9be77a51938b4df8183a34bfb026d00454f16
John <37978984+johnridesabike@users.noreply.github.com>
2018-08-19 10:54:47 -0400

    initial branch commit

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -38,11 +38,16 @@ if ( ! function_exists( 'weracoba_posted_by' ) ) :
 	/**
 	 * Prints HTML with meta information for the current author.
 	 */
-	function weracoba_posted_by() {
+	function weracoba_posted_by($with_avatar = true) {
+        if ($with_avatar) {
+            $avatar = get_avatar( get_the_author_meta( 'ID' ), 144 );
+        } else {
+            $avatar = '';
+        }
 		$byline = sprintf(
 			/* translators: %s: post author. */
 			esc_html_x( '%s', 'post author', 'weracoba' ),
-			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . get_avatar( get_the_author_meta( 'ID' ), 144 ) . esc_html( get_the_author() ) . '</a></span>'
+			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . $avatar . esc_html( get_the_author() ) . '</a></span>'
 		);
 
 		echo '<span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,43 +10,42 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<!--<div class="post_content">-->
-		<header class="entry-header">
-	    	<?php weracoba_category_list(); ?>
-			<?php
-			if ( is_singular() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			endif;
+    <?php weracoba_post_thumbnail(); ?>
+    <div class="entry-wrap">
+        <header class="entry-header">
+            <?php weracoba_category_list(); ?>
+            <?php
+            if ( is_singular() ) :
+                the_title( '<h1 class="entry-title">', '</h1>' );
+            else :
+                the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+            endif;?>
+        </header><!-- .entry-header -->
 
-			if ( 'post' === get_post_type() ) :
-				?>
-
-				<div class="entry-meta">
-					<?php weracoba_posted_by(); ?>
-					<?php weracoba_posted_on(); ?>
-					<?php weracoba_comments(); ?>
-				</div><!-- .entry-meta -->
-			<?php endif; ?>
-		</header><!-- .entry-header -->
-
-		<?php weracoba_post_thumbnail(); ?>
-
-		<div class="entry-content">
-			<?php the_excerpt(); ?>
-		</div><!-- .entry-content -->
-		<div class="continue-reading">
-			<?php
-			$read_more_link = sprintf(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				wp_kses( __( 'Continue reading %s', 'weracoba' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false)
-			);
-			 ?>
-			<a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">
-				<?php echo $read_more_link ?>
-			</a>
-		</div>
-	<!--</div> .post_content -->
+        <div class="entry-content">
+            <?php the_excerpt(); ?>
+        </div><!-- .entry-content -->
+        <footer class="entry-footer">
+            <?php
+            if ( 'post' === get_post_type() ) : ?>
+                <div class="entry-meta">
+                    <?php weracoba_posted_by(false); ?>
+                    <?php weracoba_posted_on(); ?>
+                    <?php weracoba_comments(); ?>
+                </div><!-- .entry-meta -->
+            <?php endif; ?>
+            <div class="continue-reading">
+                <?php
+                $read_more_link = sprintf(
+                    /* translators: %s: Name of current post. Only visible to screen readers */
+                    wp_kses( __( 'More %s', 'weracoba' ), array( 'span' => array( 'class' => array() ) ) ),
+                    the_title( '<span class="screen-reader-text">"', '"</span>', false)
+                );
+                 ?>
+                <a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">
+                    <?php echo $read_more_link ?>
+                </a>
+            </div>
+        </footer>
+    </div>
 </article><!-- #post-<?php the_ID(); ?> -->
