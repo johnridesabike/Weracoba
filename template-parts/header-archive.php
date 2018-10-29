4bc8c8ec953cc1223d73cbcb61200d637b284f33
John <37978984+johnridesabike@users.noreply.github.com>
2018-10-29 16:50:59 -0400

    Quick fix for attachment pages

--- template-parts/header-archive.php
+++ template-parts/header-archive.php
@@ -13,4 +13,4 @@ if ( is_active_sidebar( 'breadcrumbs-1' ) ) : ?>
         <?php dynamic_sidebar( 'breadcrumbs-1' ); ?>
     </nav>
 <?php endif;?>
-<h1 class="page-title"><?php the_archive_title(); /*single_post_title();*/ ?></h1>
+<h1 class="page-title"><?php the_archive_title(); ?></h1>
