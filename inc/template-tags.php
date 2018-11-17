2ba48c9e3883d61d95a260a9d443e7620f62a847
John <37978984+johnridesabike@users.noreply.github.com>
2018-11-17 13:46:19 -0500

    "posted on" updates, media in posts updates

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -12,25 +12,37 @@ if ( ! function_exists( 'weracoba_posted_on' ) ) :
 	 * Prints HTML with meta information for the current post-date/time.
 	 */
 	function weracoba_posted_on() {
-		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-		}
+		if ( get_the_time( 'U' ) === get_the_modified_time( 'U' ) ) {
+            $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+        } else {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+        }
+        
+        $time_string_updated = '<time class="updated" datetime="%1$s">%2$s</time>';
 
 		$time_string = sprintf( $time_string,
 			esc_attr( get_the_date( DATE_W3C ) ),
-			esc_html( get_the_date() ),
-			esc_attr( get_the_modified_date( DATE_W3C ) ),
-			esc_html( get_the_modified_date() )
+			esc_html( get_the_date() )
 		);
 
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
-			esc_html_x( '%s', 'post date', 'weracoba' ),
-			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-		);
+			esc_html_x( 'Posted on %s', 'post date', 'weracoba' ), $time_string );
 
 		echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
+        
+        if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+            $time_string_updated = sprintf( $time_string_updated,
+                esc_attr( get_the_modified_date( DATE_W3C ) ),
+                esc_html( get_the_modified_date() )
+            );
+
+            $updated_on = sprintf(
+                /* translators: %s: post date. */
+                esc_html_x( 'Updated on %s', 'updated date', 'weracoba' ), $time_string_updated );
+
+		  echo '<span class="updated-on">' . $updated_on . '</span>'; // WPCS: XSS OK.
+        }
 	}
 endif;
 
@@ -46,7 +58,7 @@ if ( ! function_exists( 'weracoba_posted_by' ) ) :
         }
 		$byline = sprintf(
 			/* translators: %s: post author. */
-			esc_html_x( '%s', 'post author', 'weracoba' ),
+			esc_html_x( 'By %s', 'post author', 'weracoba' ),
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . $avatar . esc_html( get_the_author() ) . '</a></span>'
 		);
 
