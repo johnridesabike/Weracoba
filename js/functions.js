b9a4aab0e6ada52711d6f2bc9023636fd29c5468
John <37978984+johnridesabike@users.noreply.github.com>
2019-01-30 12:56:41 -0500

    WP ESLint config

--- js/functions.js
+++ js/functions.js
@@ -5,35 +5,38 @@
  * Make the header fixed when scrolling.
  * This doesn't work on IE older than 10... for now.
  */
-(function () {
-    "use strict";
+( function() {
+	'use strict';
+	var lastScrollTop;
     function scrollTop() {
         return document.body.scrollTop || document.documentElement.scrollTop;
     }
-    var lastScrollTop = scrollTop();
-    window.onscroll = function () {
+    lastScrollTop = scrollTop();
+    window.onscroll = function() {
         var newScrollTop = scrollTop(),
-            header = document.getElementById("global-header"),
-            page = document.getElementById("page"),
-            nav = document.getElementById("site-navigation");
-        if (newScrollTop >= 30) {
-            header.classList.add('fixed-header');
-            page.classList.add('sticky-top');
+            header = document.getElementById( 'global-header' ),
+            page = document.getElementById( 'page' ),
+            nav = document.getElementById( 'site-navigation' );
+        if ( 0 <= newScrollTop ) {
+            header.classList.add( 'fixed-header' );
+            page.classList.add( 'sticky-top' );
         } else {
-			header.classList.remove('fixed-header');
-			page.classList.remove('sticky-top');
-        }
+			header.classList.remove( 'fixed-header' );
+			page.classList.remove( 'sticky-top' );
+		}
+
         // Make the header reappear if hidden on small screens
-        if (newScrollTop < lastScrollTop) {
-            header.classList.add('header-visible');
-            page.classList.add('header-visible');
+        if ( newScrollTop < lastScrollTop ) {
+            header.classList.add( 'header-visible' );
+            page.classList.add( 'header-visible' );
         } else {
+
             // Don't hide the header if the menu is open on small screens
-            if (!nav.classList.contains('toggled')) {
-                header.classList.remove('header-visible');
-                page.classList.remove('header-visible');
+            if ( ! nav.classList.contains( 'toggled' ) ) {
+                header.classList.remove( 'header-visible' );
+                page.classList.remove( 'header-visible' );
             }
         }
         lastScrollTop = newScrollTop;
     };
-}());
+}() );
