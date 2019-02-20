adb3e98a3bd46f92706ee2d3702a8109bc52acc3
John <37978984+johnridesabike@users.noreply.github.com>
2019-02-20 10:32:11 -0500

    Typography fixes

--- functions.php
+++ functions.php
@@ -225,7 +225,7 @@ add_action( 'widgets_init', 'weracoba_widgets_init' );
  * Enqueue scripts and styles.
  */
 function weracoba_scripts() {
-	wp_enqueue_style( 'weracoba-style', get_stylesheet_uri(), array(), '20190219.2', 'all' );
+	wp_enqueue_style( 'weracoba-style', get_stylesheet_uri(), array(), '20190220', 'all' );
 	wp_enqueue_style( 'weracoba-print-style', get_template_directory_uri() . '/style-print.css', array( 'weracoba-style' ), '20190217', 'print' );
 
 	/* phpcs:ignore
